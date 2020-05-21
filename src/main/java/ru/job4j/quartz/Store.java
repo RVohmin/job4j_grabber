7ccff3e5219e9eced8104364cb5c7fd8e640212a
RVohmin <vohmin77@gmail.com>
2020-05-21 11:31:01 +0300

    1.1. Job c параметрами [#280086] вынесено в классы, добавлен Store

---
+++ src/main/java/ru/job4j/quartz/Store.java
@@ -0,0 +1,34 @@
+package ru.job4j.quartz;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class Store {
+    Connect connect;
+    Schedule schedule;
+
+    public Store(Connect connect, Schedule schedule) {
+        this.connect = connect;
+        this.schedule = schedule;
+    }
+
+    public void insertDB() {
+        Connection cn = connect.init();
+        List<Long> store = schedule.shedule(cn);
+        try (PreparedStatement ps = cn.prepareStatement(
+                "INSERT INTO rabbit (created_date) VALUES (to_timestamp(?, 'YYYY/MM/dd HH:MI:SS'))")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            for (Long item : store) {
+                String startTime = sdf.format(new Date(item));
+                ps.setString(1, startTime);
+                ps.executeUpdate();
+            }
+            cn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
